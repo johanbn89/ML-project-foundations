e1c89b8a8e939eed5a90ed42cc71f0acd41a333f
Johan <johan.norlinder@auticon.com>
2026-01-26 15:46:29 +0100

    more code

---
+++ src/ml_project_foundations/models/model1.py
@@ -0,0 +1,47 @@
+"""
+Example Model 2 definition using PyTorch Lightning.
+"""
+
+import lightning.pytorch as pl
+import torch.nn.functional as F
+from omegaconf import DictConfig
+from torch import nn
+from torch.optim import Adam
+
+
+class Model1(pl.LightningModule):
+    def __init__(self, model_cfg: DictConfig):
+        super().__init__()
+        self.save_hyperparameters(model_cfg)  # Saves hyperparameters to self.hparams
+        self.layer1 = nn.Linear(model_cfg.input_dim, 64)
+        self.layer2 = nn.Linear(64, 32)
+        self.output_layer = nn.Linear(32, 1)
+        self.cfg = model_cfg
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = self.output_layer(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log("val_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log("test_loss", loss)
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=self.hparams.lr)
--- src/ml_project_foundations/runner/main.py
+++ src/ml_project_foundations/runner/main.py
@@ -2,3 +2,66 @@
 Should we really have mode branching logic in main.py?
 Could just separate into  branching_flags.py or similar.
 """
+
+import warnings
+
+import hydra
+import lightning.pytorch as pl
+from hydra.utils import instantiate
+from lightning.pytorch.loggers import MLFlowLogger
+from omegaconf import OmegaConf
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+@hydra.main(version_base=None, config_path="../config", config_name="default")
+def main(cfg):
+    print("Starting training with PyTorch Lightning")
+    print("Configuration:\n", OmegaConf.to_yaml(cfg))
+    # Think about this later
+    # seed_everything(cfg.seed)
+
+    model = instantiate(cfg.model)
+    datamodule = instantiate(cfg.data)
+
+    # This should also be instantiate(cfg.logger)?
+    # Yes, but we assume only MLFlow for now.
+    if cfg.logger.name == "mlflow":
+        logger = MLFlowLogger(
+            experiment_name=cfg.logger.experiment_name,
+            tracking_uri=cfg.logger.tracking_uri,  # Should this be env derived?
+            run_id=cfg.logger.run_name,
+        )
+
+    trainer = pl.Trainer(
+        **cfg.trainer,
+        logger=logger,
+    )
+
+    if "fit" in cfg.run.stages:
+        trainer.fit(model, datamodule=datamodule)
+
+    if "eval" in cfg.run.stages:
+        trainer.validate(
+            model,
+            datamodule=datamodule,
+            ckpt_path=cfg.run.ckpt_path,
+        )
+
+    if "test" in cfg.run.stages:
+        trainer.test(
+            model,
+            datamodule=datamodule,
+            ckpt_path=cfg.run.ckpt_path,
+        )
+
+    if "predict" in cfg.run.stages:
+        trainer.predict(
+            model,
+            datamodule=datamodule,
+            ckpt_path=cfg.run.ckpt_path,
+        )
+
+
+if __name__ == "__main__":
+    main()
